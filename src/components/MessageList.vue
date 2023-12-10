e66a05bae660d2a19925020f344057aadc1e4cf6
saJeremyQin <343220868@qq.com>
2023-12-11 10:17:59 +1030

    Display list using Vuetify

---
+++ src/components/MessageList.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h4 class="text-h4">Messages</h4>
+        <!-- <ul>
+            <li v-for="(message,index) in messages" :key="index">{{ message }}</li>
+        </ul> -->
+        <v-list>
+            <v-list-item 
+                v-for="(message,index) in messages" 
+                :key="index"
+                class="v-list-item"
+            >
+                <v-list-item-content @click.prevent="">
+                    {{ message }}
+                </v-list-item-content>
+            </v-list-item>
+        </v-list>
+    </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+    const messages = ref([
+        "first",
+        "second",
+        "Facts"
+    ]);
+    console.log(messages);
+    console.log(messages.value);
+</script>
+
+<style scoped>
+.v-list-item:hover {
+    background-color: #e0e0e0;
+}
+</style>
