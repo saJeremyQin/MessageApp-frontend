5ecb48d4d247305c8261f2387380def11d5b4098
saJeremyQin <343220868@qq.com>
2023-12-11 20:07:08 +1030

    Get messages from server

--- src/components/MessageList.vue
+++ src/components/MessageList.vue
@@ -10,23 +10,35 @@
                 :key="index"
                 class="v-list-item"
             >
-                <v-list-item-content @click.prevent="">
-                    {{ message }}
-                </v-list-item-content>
+                {{ message }}
             </v-list-item>
         </v-list>
     </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
+import axios from 'axios';
     const messages = ref([
         "first",
         "second",
         "Facts"
     ]);
-    console.log(messages);
-    console.log(messages.value);
+
+    onMounted(async () => { 
+        try {
+            const response = await axios.get("http://localhost:3000/messages");
+            messages.value = response.data;
+        } catch (error) {
+            if(error.response) {
+                console.error('Server responded with an error:', error.response.status);
+            } else if(error.request) {
+                console.error('No response received from the server');
+            } else {
+                console.error('Error in request setup:', error.message);
+            }
+        }
+    });
 </script>
 
 <style scoped>
