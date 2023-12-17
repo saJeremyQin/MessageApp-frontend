e2c1ab3f1918a086ea6046160d36ad5866fba101
saJeremyQin <343220868@qq.com>
2023-12-17 14:26:59 +1030

    before layout complete

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,25 @@
 <template>
-  <v-toolbar>
+  <!-- <v-toolbar>
     <v-btn flat to="/">Messages</v-btn>
     <v-btn flat class="ml-2" to="/newmessage">New Message</v-btn>
     <v-spacer></v-spacer>
-    <v-btn flat>Login</v-btn>
+    <v-btn flat>Register</v-btn>
+    <v-btn flat class="ml-2">Login</v-btn>   
   </v-toolbar>
-  <br>
-  <router-view />
+  <br> -->
+  <div>
+    <NavBar />
+    <br>
+    <router-view />
+  </div>
 </template>
 
 <script>
-
+import NavBar from './components/NavBar.vue'
 export default {
   name: 'App',
   components: {
-
+    NavBar,
   }
 }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 import MessageList from '../components/MessageList.vue';
 import NewMessage from '../components/NewMessage.vue';
 import MessageDetail from '../components/MessageDetail.vue';
+import UserRegister from '../components/UserRegister.vue';
 
 const routes = [
     { path:'/', component: MessageList },
     { path:'/newmessage', component: NewMessage },
-    { path:'/messages/:id', component: MessageDetail}
+    { path:'/messages/:id', component: MessageDetail},
+    { path:'/register', component: UserRegister},
 ]
 
 const router = createRouter({
